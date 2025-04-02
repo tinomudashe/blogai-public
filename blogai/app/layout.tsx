cfba86190d22d8c9900cc8115e5d4e5376506e3e
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-04-02 23:23:41 +0200

    April 2

--- blogai/app/layout.tsx
+++ blogai/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Sans as Fontsans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const fontsans = Fontsans({
+  subsets:["latin"],
+  weight:["300","400","500","600"],
+  variable:"--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontsans.variable
+        )}>
+      
         {children}
       </body>
     </html>
---
+++ blogai/components/home/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <section className="lg:max-w-6xl mx-auto flex flex-col z-0 items-center py-28 
+    sm:pt-32 transition-all animate-in">
+        <h1>
+            Turn your words into captivating blog posts
+        </h1>
+
+    </section>
+  )
+}
