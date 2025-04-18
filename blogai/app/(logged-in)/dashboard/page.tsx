0f58ce09bbefc8a11110059a8278b182e99e8210
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-04-18 15:04:08 +0200

    another edit

--- blogai/app/(logged-in)/dashboard/page.tsx
+++ blogai/app/(logged-in)/dashboard/page.tsx
@@ -1,94 +1,94 @@
 
-import getDBConnection from "@/lib/db"
-import { currentUser } from "@clerk/nextjs/server";
-import { doesUserExist, getPlanType, updateUser } from "@/lib/user-helpers";
 import { BgGradient } from "@/components/common/bg-gradient";
 import { Badge } from "@/components/ui/badge";
 import UpgradeYourPlan from "@/components/Upload/upgradeYourPlan";
 import UploadForm from "@/components/Upload/uploadForm";
+
+import getDbConnection from "@/lib/db";
+import {
+  doesUserExist,
+  getPlanType,
+  hasCancelledSubscription,
+  updateUser,
+} from "@/lib/user-helpers";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
+export default async function Dashboard() {
+  const clerkUser = await currentUser();
 
+  if (!clerkUser) {
+    return redirect("/sign-in");
+  }
 
-export default async function Dashboard(){
-    
-    const clerkUser = await currentUser();
-    const email = clerkUser?.emailAddresses?.[0].emailAddress ?? "";
+  const email = clerkUser?.emailAddresses?.[0].emailAddress ?? "";
 
-    if(!clerkUser){
-        return redirect("/sign-in")
+  const sql = await getDbConnection();
+
+  //updatethe user id
+  let userId = null;
+  let priceId = null;
+
+  const hasUserCancelled = await hasCancelledSubscription(sql, email);
+  const user = await doesUserExist(sql, email);
+
+  if (user) {
+    //update the user_id in users table
+    userId = clerkUser?.id;
+    if (userId) {
+      await updateUser(sql, userId, email);
     }
 
-    const sql = await getDBConnection();
+    priceId = user[0].price_id;
+  }
 
+  const { id: planTypeId = "starter", name: planTypeName } =
+    getPlanType(priceId);
 
-    let userId = null;
-    let priceId = null; 
+  const isBasicPlan = planTypeId === "basic";
+  const isProPlan = planTypeId === "pro";
 
-    
+  // check number of posts per plan
+  const posts = await sql`SELECT * FROM posts WHERE user_id = ${userId}`;
 
-  
-    const user = await doesUserExist(sql,email);
+  const isValidBasicPlan = isBasicPlan && posts.length < 3;
 
+  return (
+    <BgGradient>
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Badge className="bg-gradient-to-r from-purple-700 to-pink-800 text-white px-4 py-1 text-lg font-semibold capitalize">
+            {planTypeName} Plan
+          </Badge>
 
-    if(user){
+          <h2 className="capitalize text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Start creating amazing content
+          </h2>
 
-        userId = clerkUser?.id;
+          <p className="mt-2 text-lg leading-8 text-gray-600 max-w-2xl text-center">
+            Upload your audio or video file and let our AI do the magic!
+          </p>
 
-        if(userId){
-            await updateUser(sql, userId, email);
-        }
-        
+          {(isBasicPlan || isProPlan) && (
+            <p className="mt-2 text-lg leading-8 text-gray-600 max-w-2xl text-center">
+              You get{" "}
+              <span className="font-bold text-amber-600 bg-amber-100 px-2 py-1 rounded-md">
+                {isBasicPlan ? "3" : "Unlimited"} blog posts
+              </span>{" "}
+              as part of the{" "}
+              <span className="font-bold capitalize">{planTypeName}</span> Plan.
+            </p>
+          )}
 
-        priceId = user[0].price_id;
-       
-    }
-    const {id:planTypeId = "starter", name:planTypeName} = getPlanType(priceId);
-        
-
-
-
-    const isBasicPlan = planTypeId  === "basic";
-    const isProPlan = planTypeId === "pro";
-
-    const posts = await sql`SELECT * FROM posts WHERE user_id = ${userId}`;
-
-    const isValidBasicPlan = isBasicPlan && posts.length < 3;
-
-    
-    return (
-        <BgGradient>
-            <div className="mx-auto max-w-7xl px-6 lg:py-20 py-14 lg:px-8">
-                <div className="flex flex-col items-center justify-center gap-6 text-center">
-                    <Badge className="bg-gradient-to-r from-purple-700 to-pink-800 text-white 
-                        px-4 py-1 text-lg font-semibold capitalize rounded-2xl">
-                        {planTypeName} Plan
-                    </Badge>
-                <h2 className="capitalize text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    Start Create amazing content
-                </h2>
-
-                <p className="mt-2 text-lg leading-8 max-w-2xl text-gray-600 text-center">
-                    Upload your audio or the video file and let our AI do the magic
-                </p>
-
-                {(isBasicPlan || isProPlan) && (
-                    <p className="mt-2 text-lg leading-8 text-gray-600 max-w-2xl text-center">
-                    You get{" "}
-                    <span className="font-bold text-amber-600 bg-amber-100 px-2 py-1 rounded-md">
-                        {isBasicPlan ? "3" : "Unlimited"} blog posts
-                    </span>{" "}
-                    as part of the{" "}
-                    <span className="font-bold capitalize">{planTypeName}</span> Plan.
-                    </p>
-                )}
-                {isValidBasicPlan||isProPlan? <UpgradeYourPlan/>:(
-                        <UploadForm/>
-                    
-                     )}
-                
-                </div>
-            </div>
-        </BgGradient>
-    );
+          {isValidBasicPlan || isProPlan ? (
+            <BgGradient>
+              <UploadForm />
+            </BgGradient>
+          ) : (
+            <UpgradeYourPlan />
+          )}
+        </div>
+      </div>
+    </BgGradient>
+  );
 }
--- blogai/app/api/payments/route.ts
+++ blogai/app/api/payments/route.ts
@@ -1,62 +1,56 @@
-import { handleCheckoutSessionCompleted, handleSubscriptionDeleted } from "@/lib/payment-helpers";
-import { NextRequest, NextResponse } from "next/server";
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
-
-
-export async function POST(req:NextRequest){
-
+import {
+    handleCheckoutSessionCompleted,
+    handleSubscriptionDeleted,
+  } from "@/lib/payment-helpers";
+  import { NextRequest, NextResponse } from "next/server";
+  import Stripe from "stripe";
+  
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  
+  export async function POST(req: NextRequest) {
+    //webhook functionality
     const payload = await req.text();
-    const sig = req.headers.get
-    ('stripe-signature');
-
+  
+    const sig = req.headers.get("stripe-signature");
+  
     let event;
-
+  
     try {
-        event = stripe.webhooks.constructEvent
-        (payload,
-             sig!,
-             process.env.STRIPE_WEBHOOK_KEY!);
-    
-
-    switch (event.type){
-        
-        case "checkout.session.completed":{
-
-            const session = await stripe.checkout.sessions.retrieve(
-                event.data.object.id,
-                {
-                    expand: ["line_items"],
-                }
-            );
-            console.log({session});
-
-            await handleCheckoutSessionCompleted({session,stripe})
-
-            //connect to db create or update user
-            break;
+      event = stripe.webhooks.constructEvent(
+        payload,
+        sig!,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
+  
+      // Handle the event
+      switch (event.type) {
+        case "checkout.session.completed": {
+          const session = await stripe.checkout.sessions.retrieve(
+            event.data.object.id,
+            {
+              expand: ["line_items"],
+            }
+          );
+          console.log({ session });
+  
+          //connect to the db create or update user
+          await handleCheckoutSessionCompleted({ session, stripe });
+          break;
         }
-        case "customer.subscription.deleted":{
-
-            const subscriptionId = event.data.object.id;
-            const subscription = await stripe.
-            subscriptions.retrieve(subscriptionId);
-            console.log({subscription});
-
-            await handleSubscriptionDeleted({ subscriptionId, stripe})
-
-            break;
+        case "customer.subscription.deleted": {
+          // connect to db
+          const subscriptionId = event.data.object.id;
+  
+          await handleSubscriptionDeleted({ subscriptionId, stripe });
+          break;
         }
         default:
-            console.log(`Unhandled event type ${event.type}`);
+          console.log(`Unhandled event type ${event.type}`);
+      }
+      return NextResponse.json({
+        status: "success",
+      });
+    } catch (err) {
+      return NextResponse.json({ status: "Failed", err });
     }
-} 
-    catch (err){
-        return NextResponse.json({status:"Failed",err})
-    }
-    return NextResponse.json({
-        status:"success",
-    });
-}
+  }
