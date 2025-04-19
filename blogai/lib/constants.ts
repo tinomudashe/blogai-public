4fd773f07ee92b7217cddf0c50c843f5790372d8
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-04-19 16:41:00 +0200

    Production fix 2

--- blogai/lib/constants.ts
+++ blogai/lib/constants.ts
@@ -8,7 +8,7 @@
             price:"10",
             items:["3 Blog Posts","3 Transcription" ],
             paymentLink: "https://buy.stripe.com/cN2g1haz42Ix568144",
-            priceId:process.env.NODE_ENV === 'development'? "price_1RCsYi2MwwfzXHrFumG2fZxY":"price_1RCspx2MwwfzXHrFXnJeen7A"
+            priceId:"price_1RCspx2MwwfzXHrFXnJeen7A"
         },
         {   
             id:'pro',
@@ -17,12 +17,9 @@
             price:"19.99",
             items:["unlimited Blog Posts","unlimited Transcriptions" ],
             paymentLink: "https://buy.stripe.com/28o16n7mSbf3aqs8wx",
-            priceId:process.env.NODE_ENV === 'development'?"price_1RCsaS2MwwfzXHrFcUOaJlhh":"price_1RCspx2MwwfzXHrFLX9WKswJ"
+            priceId:"price_1RCspx2MwwfzXHrFLX9WKswJ"
         },
         
             
     ]
-    export const ORIGIN_URL = 
-            process.env.NODE_ENV === 'development'
-            ?"localhost:3000"
-            :"https://blogai.blog"
+    export const ORIGIN_URL ="https://blogai.blog"
