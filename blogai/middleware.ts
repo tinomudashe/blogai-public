a0b04e13ee7d3ee61449127b08b71c5fd91f9600
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-04-19 20:01:33 +0200

    production edit 1

--- blogai/middleware.ts
+++ blogai/middleware.ts
@@ -4,7 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/posts(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (!isProtectedRoute(req)) {
+  if (isProtectedRoute(req)) {
     auth.protect()
   }
 })
