8239278e4938ddd76e6b9d80a6ae72276156a28f
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-04-19 17:38:32 +0200

    production fix 4

--- blogai/middleware.ts
+++ blogai/middleware.ts
@@ -1,12 +1,11 @@
 
-
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)','/api(.*)',])
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/posts(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+export default clerkMiddleware((auth, req) => {
+  if (!isProtectedRoute(req)) {
+    auth.protect()
   }
 })
 
